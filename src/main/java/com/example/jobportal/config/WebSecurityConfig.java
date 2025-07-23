22de5cf15c68538a48e5fa70605c19d432c4b1da
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2025-07-23 15:54:18 +0200

    feat: Implement UserDetails, UserDetailsService and Security Configuration

---
+++ src/main/java/com/example/jobportal/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.jobportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final String[] publicUrl = {"/",
+            "/global-search/**",
+            "/register",
+            "/register/**",
+            "/webjars/**",
+            "/resources/**",
+            "/assets/**",
+            "/css/**",
+            "/summernote/**",
+            "/js/**",
+            "/*.css",
+            "/*.js",
+            "/*.js.map",
+            "/fonts**",
+            "/favicon.ico",
+            "/resources/**",
+                "/error"};
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http){
+        try {
+            http.authorizeHttpRequests(auth->{
+                auth.requestMatchers(publicUrl).permitAll();
+                auth.anyRequest().authenticated();
+            });
+            return http.build();
+        }
+        catch (Exception e){
+            throw new IllegalStateException("Failed to configure SecurityFilterChain", e);
+        }
+    }
+
+}
