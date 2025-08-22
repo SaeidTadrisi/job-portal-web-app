d24848acaf724ef10f6d99447ba2cedf68931d7d
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2025-08-22 12:26:20 +0200

    fix: jobSeekerApplyRepository and jobSeekerApplyService method names changed.

--- src/main/java/com/example/jobportal/services/JobSeekerApplyService.java
+++ src/main/java/com/example/jobportal/services/JobSeekerApplyService.java
@@ -4,6 +4,7 @@ import com.example.jobportal.entity.JobPostActivity;
 import com.example.jobportal.entity.JobSeekerApply;
 import com.example.jobportal.entity.JobSeekerProfile;
 import com.example.jobportal.repository.JobSeekerApplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,15 +14,20 @@ public class JobSeekerApplyService {
 
     private final JobSeekerApplyRepository jobSeekerApplyRepository;
 
+    @Autowired
     public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
         this.jobSeekerApplyRepository = jobSeekerApplyRepository;
     }
 
     public List<JobSeekerApply> getCandidateJobs(JobSeekerProfile userAccountId) {
-        return jobSeekerApplyRepository.findByUserId(userAccountId);
+        return jobSeekerApplyRepository.findByJobSeekerProfile(userAccountId);
     }
 
     public List<JobSeekerApply> getJobCandidates(JobPostActivity jobId) {
-        return jobSeekerApplyRepository.findByJob(jobId);
+        return jobSeekerApplyRepository.findByJobPostActivity(jobId);
+    }
+
+    public void save(JobSeekerApply jobSeekerApply) {
+        jobSeekerApplyRepository.save(jobSeekerApply);
     }
 }
