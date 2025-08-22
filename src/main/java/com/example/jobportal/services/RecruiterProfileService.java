f3309fe06a297e9b68b4b9cdbdd10244ce19f7bf
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2025-08-22 12:29:43 +0200

    feat: getCurrentProfile added to jobSeekerProfileService and RecruiterProfileService.

--- src/main/java/com/example/jobportal/services/RecruiterProfileService.java
+++ src/main/java/com/example/jobportal/services/RecruiterProfileService.java
@@ -2,7 +2,12 @@ package com.example.jobportal.services;
 
 
 import com.example.jobportal.entity.RecruiterProfile;
+import com.example.jobportal.entity.Users;
 import com.example.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +16,12 @@ import java.util.Optional;
 public class RecruiterProfileService {
 
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersService usersService;
 
 
-    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository, UsersService usersService) {
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersService = usersService;
     }
 
     public Optional<RecruiterProfile> getRecruiterProfile(Integer id){
@@ -23,5 +30,15 @@ public class RecruiterProfileService {
 
     public RecruiterProfile save(RecruiterProfile recruiterProfile) {
         return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersService.getUserByEmail(authentication.getName());
+            Optional<RecruiterProfile> recruiterProfile = getRecruiterProfile(user.getUserId());
+            return recruiterProfile.orElse(null);
+        }
+        return null;
     }
 }
